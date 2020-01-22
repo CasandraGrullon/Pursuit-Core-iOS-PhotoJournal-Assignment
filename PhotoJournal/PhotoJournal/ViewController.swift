7b50597fb105b649661487868830f4e340681561
Casandra <casandragrullon@pursuit.org>
2020-01-22 09:38:22 -0500

    started ui

---
+++ PhotoJournal/PhotoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournal
+//
+//  Created by casandra grullon on 1/22/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
